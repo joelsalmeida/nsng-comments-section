e4ed056031de6f520e6e56fec24635c7ef063a09
Joel Almeida <joel.almeida.sa@gmail.com>
2024-07-13 13:10:50 -0300

    test: Add unit tests for CommentController patch method

--- src/models/comment/comment.controller.spec.ts
+++ src/models/comment/comment.controller.spec.ts
@@ -3,16 +3,20 @@ import { CommentController } from './comment.controller';
 import { Comment } from './comment.entity';
 import { CommentService } from './comment.service';
 
-const CREATE_SUCCESS_MESSAGE = 'Comment posted successfully';
-const FIND_ALL_SUCCESS_MESSAGE = 'Comments fetched Successfully';
-const LIKED_SUCCESS_MESSAGE = 'Comment liked successfully.';
-const UNLED_SUCCESS_MESSAGE = 'Comment "unliked" successfully';
-
 const CREATE_PARAM = {
   sender: 'sender-id',
   body: 'comment body',
 };
 const LIKE_PARAM = { sender: 'sender-id', comment: 'comment-id' };
+const PATCH_ID_PARAM = 'patch-comment-id';
+const PATCH_PARAM = { body: 'new comment body' };
+
+const CREATE_SUCCESS_MESSAGE = 'Comment posted successfully';
+const FIND_ALL_SUCCESS_MESSAGE = 'Comments fetched Successfully';
+const LIKED_SUCCESS_MESSAGE = 'Comment liked successfully';
+const UNLED_SUCCESS_MESSAGE = 'Comment "unliked" successfully';
+const PATCH_SUCCESS_MESSAGE = 'Comment patched successfully';
+const PATCH_NOT_FOUND_MESSAGE = `Comment not found with id: ${PATCH_ID_PARAM}`;
 
 describe('CommentController', () => {
   let commentController: CommentController;
@@ -75,6 +79,66 @@ describe('CommentController', () => {
       expect(await commentController.create(CREATE_PARAM)).toStrictEqual(
         expectedResult,
       );
+    });
+  });
+
+  describe('patch', () => {
+    it('on success, should return "success: true, success message and service data"', async () => {
+      const mockCommentServiceReturnedData = {
+        sender: 'sender-id',
+        body: 'comment body',
+        likes: [],
+      };
+
+      jest
+        .spyOn(commentService, 'patch')
+        .mockImplementation(
+          async () => mockCommentServiceReturnedData as Comment,
+        );
+
+      const expectedResult = {
+        success: true,
+        message: PATCH_SUCCESS_MESSAGE,
+        data: mockCommentServiceReturnedData,
+      };
+
+      expect(
+        await commentController.patch('comment-id', PATCH_PARAM),
+      ).toStrictEqual(expectedResult);
+    });
+
+    it('if comment not found (service return null), should return "success: false and error message with id"', async () => {
+      const mockCommentServiceReturnedData = null;
+
+      jest
+        .spyOn(commentService, 'patch')
+        .mockImplementation(async () => mockCommentServiceReturnedData);
+
+      const expectedResult = {
+        success: false,
+        message: PATCH_NOT_FOUND_MESSAGE,
+      };
+
+      expect(
+        await commentController.patch(PATCH_ID_PARAM, PATCH_PARAM),
+      ).toStrictEqual(expectedResult);
+    });
+
+    it('on error, should return "success: false and error message"', async () => {
+      const mockCommentServiceReturnedData = new Error('Internal Server Error');
+
+      jest.spyOn(commentService, 'patch').mockImplementation(async () => {
+        throw mockCommentServiceReturnedData;
+      });
+
+      const expectedResult = {
+        success: false,
+        message: mockCommentServiceReturnedData.message,
+      };
+
+      expect(
+        await commentController.patch('comment-id', PATCH_PARAM),
+      ).toStrictEqual(expectedResult);
     });
   });
 
