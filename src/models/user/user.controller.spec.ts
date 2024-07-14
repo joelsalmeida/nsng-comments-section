3919883865420b0422ea60455d24979bbac5ddd8
Joel Almeida <joel.almeida.sa@gmail.com>
2024-07-14 11:40:22 -0300

    test: Add unit tests for UserController findOne method

--- src/models/user/user.controller.spec.ts
+++ src/models/user/user.controller.spec.ts
@@ -2,10 +2,14 @@ import { Test } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
+const FIND_ONE_ID_PARAM = 'find-one-user-id';
+
 const CREATE_SUCCESS_MESSAGE = 'User created successfully';
 const CREATE_PARAM = {
   username: 'my-username',
 };
+const FIND_ONE_SUCCESS_MESSAGE = 'User fetched successfully';
+const FIND_ONE_NOT_FOUND_MESSAGE = `User not found with id: ${FIND_ONE_ID_PARAM}`;
 
 describe('UserController', () => {
   let userController: UserController;
@@ -63,6 +67,63 @@ describe('UserController', () => {
       };
 
       expect(await userController.create(CREATE_PARAM)).toStrictEqual(
+        expectedResult,
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('on success, should return "success: true, success message and service data"', async () => {
+      const mockCommentServiceReturnedData = {
+        username: 'find-username',
+        id: 'find-id',
+      };
+
+      jest
+        .spyOn(userService, 'findOne')
+        .mockImplementation(async () => mockCommentServiceReturnedData);
+
+      const expectedResult = {
+        success: true,
+        message: FIND_ONE_SUCCESS_MESSAGE,
+        data: mockCommentServiceReturnedData,
+      };
+
+      expect(await userController.findOne('comment-id')).toStrictEqual(
+        expectedResult,
+      );
+    });
+
+    it('if user not found (service return null), should return "success: false and error message with id"', async () => {
+      const mockCommentServiceReturnedData = null;
+
+      jest
+        .spyOn(userService, 'findOne')
+        .mockImplementation(async () => mockCommentServiceReturnedData);
+
+      const expectedResult = {
+        success: false,
+        message: FIND_ONE_NOT_FOUND_MESSAGE,
+      };
+
+      expect(await userController.findOne(FIND_ONE_ID_PARAM)).toStrictEqual(
+        expectedResult,
+      );
+    });
+
+    it('on error, should return "success: false and error message"', async () => {
+      const mockCommentServiceReturnedData = new Error('Internal Server Error');
+
+      jest.spyOn(userService, 'findOne').mockImplementation(async () => {
+        throw mockCommentServiceReturnedData;
+      });
+
+      const expectedResult = {
+        success: false,
+        message: mockCommentServiceReturnedData.message,
+      };
+
+      expect(await userController.findOne('comment-id')).toStrictEqual(
         expectedResult,
       );
     });
