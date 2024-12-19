9200d23f5c767ecdf6a3744e59ad55de73765cd6
Joel Almeida <joel.almeida.sa@gmail.com>
2024-12-19 11:43:56 -0300

    feat: Integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocumentConfig = new DocumentBuilder()
+    .setTitle('NSNG Comments Section API')
+    .setDescription('The NSNG Comments Section API description')
+    .setVersion('1.0')
+    .addTag('nsng')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    swaggerDocumentConfig,
+  );
+
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   app.enableCors();
 
@@ -11,4 +26,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
